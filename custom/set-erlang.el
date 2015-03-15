fc3979387cfb07c91e37fa97e15b32eb65c600c5
Ed Maphis <emaphis85@gmail.com>
2015-03-15 17:28:37 -0400

    more settings for erlang mode

--- custom/set-erlang.el
+++ custom/set-erlang.el
@@ -2,11 +2,25 @@
 ;; provides erlang customizations
 ;; Copyright © 2015 Ed Maphis
 
+;; some hints from Alex Ott:
+;; http://alexott.net/en/writings/emacs-devenv/EmacsErlang.html
+
 (setq load-path (cons  "C:\\erl6.3\\lib\\tools-2.7.1\\emacs"
                        load-path))
 (setq erlang-root-dir "C:\\erl6.3")
 (setq exec-path (cons "C:\\erl6.3\bin" exec-path))
 (require 'erlang-start)
+
+(defun my-erlang-mode-hook ()
+        ;; when starting an Erlang shell in Emacs, default in the node name
+        (setq inferior-erlang-machine-options '("-sname" "emacs"))
+        ;; add Erlang functions to an imenu menu
+        (imenu-add-to-menubar "imenu")
+        ;; customize keys
+        (local-set-key [return] 'newline-and-indent))
+
+;; Some Erlang customizations
+(add-hook 'erlang-mode-hook 'my-erlang-mode-hook)
 
 (provide 'set-erlang)
 ;; set-erlang.el ends here
